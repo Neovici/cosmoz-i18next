ab5f97f69135c2a43bded5b68a6a9e76e5cdca90
Patrik Kullman <patrik.kullman@neovici.se>
2015-11-09 15:05:54 +0100

    Support translation update
    - Rerender any translatable strings by supplying the a 't' kicker
    - Once translations are updated, all translatables' kickers are reset

--- cosmoz-i18next.js
+++ cosmoz-i18next.js
@@ -1,7 +1,75 @@
 /*global Cosmoz, i18n, Polymer */
+
+if (typeof Cosmoz === 'undefined') {
+	var Cosmoz = {};
+}
+
 (function () {
 
 	"use strict";
+
+	var translationElements = [];
+
+	Cosmoz.TranslatableBehavior = {
+		properties: {
+			t: {
+				type: Object,
+				value: function () {
+					return {};
+				}
+			}
+		},
+		_argumentsToObject: function (args, skipnum) {
+			var argsArray = Array.prototype.slice.call(args, skipnum);
+			return this._arrayToObject(argsArray);
+		},
+		_arrayToObject: function (array) {
+			var ctx = this,
+				object = {};
+
+			array.forEach(function (item, index) {
+				if (object.count === undefined && typeof item === "number") {
+					object.count = item;
+				}
+				// Don't send the 't' kicker to i18n for arguments
+				if (item !== ctx.t) {
+					object[index] = item;
+				}
+			});
+			return object;
+		},
+		_: function (key) {
+			var args = this._argumentsToObject(arguments, 1);
+			// Don't make i18next fetch more translations
+			delete args.count;
+			return i18n.t(key, args);
+		},
+		attached: function () {
+			translationElements.push(this);
+		},
+		detached: function () {
+			var i = translationElements.indexOf(this);
+			if (i >= 0) {
+				translationElements.splice(i, 1);
+			}
+		},
+		ngettext: function (singular, plural) {
+			var args = this._argumentsToObject(arguments, 2);
+			return i18n.t(singular, args);
+		},
+		pgettext: function (context, key) {
+			var args = this._argumentsToObject(arguments, 2);
+			args.context = context;
+			// Don't make i18next fetch more translations
+			delete args.count;
+			return i18n.t(key, args);
+		},
+		npgettext: function (context, singular, plural) {
+			var args = this._argumentsToObject(arguments, 3);
+			args.context = context;
+			return i18n.t(singular, args);
+		}
+	};
 
 	Polymer({
 		behaviors: [
@@ -36,6 +104,9 @@
 		},
 		_setTranslations: function () {
 			i18n.addResources(this.language, this.namespace, this.translations);
+			translationElements.forEach(function (element) {
+				element.t = {};
+			});
 		},
 		ready: function () {
 			i18n.init({
