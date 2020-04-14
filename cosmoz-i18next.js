a4cf2f9a09b9583f895d4bcaf79c50ec11ff93b6
Cristian Necula <virusz@gmail.com>
2020-04-14 08:48:33 +0300

    chore: fix lint errors

--- cosmoz-i18next.js
+++ cosmoz-i18next.js
@@ -88,136 +88,142 @@ const
 	loadTranslations = (lang, namespace, translations) => {
 		i18n.init({ resources: {}});
 		i18n.addResourceBundle(lang, namespace, translations);
-	};
-
-export const translatable = dedupingMixin(baseClass => class extends baseClass {
-	/**
-	 * Get mixin properties.
-	 * @returns {object} Mixin properties.
-	*/
-	static get properties() {
-		return {
-			t: {
-				type: Object,
-				value() {
-					return {};
+	},
+	translatable = dedupingMixin(baseClass => class extends baseClass {
+		/**
+		 * Get mixin properties.
+		 * @returns {object} Mixin properties.
+		 */
+		static get properties() {
+			return {
+				t: {
+					type: Object,
+					value() {
+						return {};
+					}
 				}
-			}
-		};
-	}
-	_filterT(args) {
-		return args.filter(item => item !== this.t);
-	}
-	/**
-	 * Convenience method for gettext. Translates a text.
-	 *
-	 * @param {string} key Translation key.
-	 * @returns {string} Translated text.
-	 */
-	_() {
-		return gettext.apply(null, this._filterT([...arguments]));
-	}
-	/**
-	 * Runs when connected.
-	 * @returns {void}
-	 */
-	connectedCallback() {
-		super.connectedCallback();
-		translationElements.push(this);
-	}
-	/**
-	 * Runs when disconnected.
-	 * @returns {void}
-	 */
-	disconnectedCallback() {
-		super.disconnectedCallback();
-		const i = translationElements.indexOf(this);
-		if (i >= 0) {
-			translationElements.splice(i, 1);
+			};
 		}
-	}
-	/**
-	 * Translates a text.
-	 *
-	 * Example of basic translation:
-	 * `_(string, t)`
-	 *	 <div>{{ _(‘My translation’, t) }}</div>
-	 *
-	 * Example of basic translation with interpolation:
-	 * `_(string, [args], t)`
-	 *	 <div>{{ _(‘Hello {0}’, user.name, t) }}</div>
-	 *
-	 * @param {string} key Text to translate.
-	 * @param {object} t Behavior t object.
-	 * @return {string} Translated text.
-	 */
-	gettext() {
-		return gettext.apply(null, this._filterT([...arguments]));
-	}
-	/**
-	 * Plural version of gettext. Translates a text to the current locale
-	 * using the first numeric argument after the two first arguments to
-	 * determine if output should be singular or plural.
-	 *
-	 * Example of translation in singular or plural:
-	 * `ngettext(singular, plural, count, t)`
-	 * <div>{{ ngettext(‘My translation’,
-	 *	 ‘My translations’, count, t) }}</div>
-	 *
-	 * Example of translation in singular or plural with interpolation:
-	 * `ngettext(singular, plural, [count and other args], t)`
-	 * <div>{{ ngettext(‘My translation for “{1}”’,
-	 *	 ‘My {0} translations for “{1}”’, count, ‘hello’, t) }}</div>
-	 *
-	 * @param {string} singular Singular text variant.
-	 * @param {string} plural Plural text variant.
-	 * @return {string} Translated text.
-	 */
-	ngettext() {
-		return ngettext.apply(null, this._filterT([...arguments]));
-	}
-	/**
-	 * Translates a text using a specific context.
-	 *
-	 * Example of translation with context:
-	 * `pgettext(context, ‘text’, t)`
-	 *	 <div>{{ pgettext(‘Cancel Invoice’, ‘Cancel’, t) }}</div>
-	 *
-	 * Example of translation including context with interpolation:
-	 * `pgettext(context, ‘text’, [args], t)`
-	 * <div>{{ pgettext(‘Cancel Invoice’, ‘Cancel {0}’,
-	 *	 document.type, t) }}</div>
-	 *
-	 * @param {string} context Context text.
-	 * @param {string} key Text to translate.
-	 * @return {string} Translated text.
-	 */
-	pgettext() {
-		return pgettext.apply(null, this._filterT([...arguments]));
-	}
-	/**
-	 * Translates a text in singular or plural with a specific context.
-	 *
-	 * Example of translation in singular or plural with context:
-	 * `npgettext(context, singular, plural, count, t)`
-	 * <div>{{ npgettext('Cancel invoice', ‘My cancellation’,
-	 *	 ‘My {0} cancellations’, count, t) }}</div>
-	 *
-	 * Example of translation in singular or plural with context and
-	 * interpolation:
-	 * `npgettext(context, singular, plural, count, t)`
-	 * <div>{{ npgettext('Cancel invoice', ‘My {1} cancellation’,
-	 *	 ‘My {0} {1} cancellations’, count, document.type, t) }}</div>
-	 *
-	 * @param {string} context Context text.
-	 * @param {string} singular Singular text variant.
-	 * @param {string} plural Plural text variant.
-	 * @return {string} Translated text.
-	 */
-	npgettext() {
-		return npgettext.apply(null, this._filterT([...arguments]));
-	}
-});
+		_filterT(args) {
+			return args.filter(item => item !== this.t);
+		}
+
+		/**
+		 * Convenience method for gettext. Translates a text.
+		 *
+		 * @param {string} key Translation key.
+		 * @returns {string} Translated text.
+		 */
+		_() {
+			return gettext.apply(null, this._filterT([...arguments]));
+		}
+
+		/**
+		 * Runs when connected.
+		 * @returns {void}
+		 */
+		connectedCallback() {
+			super.connectedCallback();
+			translationElements.push(this);
+		}
+
+		/**
+		 * Runs when disconnected.
+		 * @returns {void}
+		 */
+		disconnectedCallback() {
+			super.disconnectedCallback();
+			const i = translationElements.indexOf(this);
+			if (i >= 0) {
+				translationElements.splice(i, 1);
+			}
+		}
+
+		/**
+		 * Translates a text.
+		 *
+		 * Example of basic translation:
+		 * `_(string, t)`
+		 *	 <div>{{ _(‘My translation’, t) }}</div>
+		 *
+		 * Example of basic translation with interpolation:
+		 * `_(string, [args], t)`
+		 *	 <div>{{ _(‘Hello {0}’, user.name, t) }}</div>
+		 *
+		 * @param {string} key Text to translate.
+		 * @param {object} t Behavior t object.
+		 * @return {string} Translated text.
+		 */
+		gettext() {
+			return gettext.apply(null, this._filterT([...arguments]));
+		}
+
+		/**
+		 * Plural version of gettext. Translates a text to the current locale
+		 * using the first numeric argument after the two first arguments to
+		 * determine if output should be singular or plural.
+		 *
+		 * Example of translation in singular or plural:
+		 * `ngettext(singular, plural, count, t)`
+		 * <div>{{ ngettext(‘My translation’,
+		 *	 ‘My translations’, count, t) }}</div>
+		 *
+		 * Example of translation in singular or plural with interpolation:
+		 * `ngettext(singular, plural, [count and other args], t)`
+		 * <div>{{ ngettext(‘My translation for “{1}”’,
+		 *	 ‘My {0} translations for “{1}”’, count, ‘hello’, t) }}</div>
+		 *
+		 * @param {string} singular Singular text variant.
+		 * @param {string} plural Plural text variant.
+		 * @return {string} Translated text.
+		 */
+		ngettext() {
+			return ngettext.apply(null, this._filterT([...arguments]));
+		}
+
+		/**
+		 * Translates a text using a specific context.
+		 *
+		 * Example of translation with context:
+		 * `pgettext(context, ‘text’, t)`
+		 *	 <div>{{ pgettext(‘Cancel Invoice’, ‘Cancel’, t) }}</div>
+		 *
+		 * Example of translation including context with interpolation:
+		 * `pgettext(context, ‘text’, [args], t)`
+		 * <div>{{ pgettext(‘Cancel Invoice’, ‘Cancel {0}’,
+		 *	 document.type, t) }}</div>
+		 *
+		 * @param {string} context Context text.
+		 * @param {string} key Text to translate.
+		 * @return {string} Translated text.
+		 */
+		pgettext() {
+			return pgettext.apply(null, this._filterT([...arguments]));
+		}
+
+		/**
+		 * Translates a text in singular or plural with a specific context.
+		 *
+		 * Example of translation in singular or plural with context:
+		 * `npgettext(context, singular, plural, count, t)`
+		 * <div>{{ npgettext('Cancel invoice', ‘My cancellation’,
+		 *	 ‘My {0} cancellations’, count, t) }}</div>
+		 *
+		 * Example of translation in singular or plural with context and
+		 * interpolation:
+		 * `npgettext(context, singular, plural, count, t)`
+		 * <div>{{ npgettext('Cancel invoice', ‘My {1} cancellation’,
+		 *	 ‘My {0} {1} cancellations’, count, document.type, t) }}</div>
+		 *
+		 * @param {string} context Context text.
+		 * @param {string} singular Singular text variant.
+		 * @param {string} plural Plural text variant.
+		 * @return {string} Translated text.
+		 */
+		npgettext() {
+			return npgettext.apply(null, this._filterT([...arguments]));
+		}
+	});
 
 /**
  * `<cosmoz-i18next>` is a translation component based on i18next
@@ -298,5 +304,6 @@ export {
 	ngettext,
 	pgettext,
 	npgettext,
-	loadTranslations
+	loadTranslations,
+	translatable
 };
